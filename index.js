c76be7358d3faf1a1bf999405bbdb9e048c12d90
Connor Duggan <connor.duggan28@yahoo.com>
2022-12-17 02:26:46 -0500

    more images

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function newImage(src, left, bottom) {
 
 newImage('green-character.gif', '100px', '100px')
 newImage('pine-tree.png', '450px', '200px')
+newImage('tree.png', '200px', '300px')
+newImage('pillar.png', '350px', '100px')
+newImage('crate.png', '150px', '200px')
+newImage('well.png', '500px', '425px')
