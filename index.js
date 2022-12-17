4bd6362bd6a8869ad61ccd6f7174e230af02e4d4
Connor Duggan <connor.duggan28@yahoo.com>
2022-12-17 02:26:13 -0500

    image function

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(src, left, bottom) {
+    let img = document.createElement('img')
+    img.src = 'assets/' + src
+    img.style.position = 'fixed'
+    img.style.left = left
+    img.style.bottom = bottom
+    document.body.append(img)
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+}
+
+newImage('green-character.gif', '100px', '100px')
+newImage('pine-tree.png', '450px', '200px')
